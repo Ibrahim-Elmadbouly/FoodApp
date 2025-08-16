aff416774eb744d023aac6a062a687ce30955728
Ibrahim-Elmadbouly <ibrahim.elmadbouly@gmail.com>
2025-08-16 03:38:46 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createHashRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout/Layout';
 import Home from './components/Home/Home';
 import RecipeDetails from './components/RecipeDetails/RecipeDetails';
 
 
-const router = createBrowserRouter([
+const router = createHashRouter([
 
     {path: '' , element: <Layout/>, children: [
         {path: '/', element: <Home/>},
